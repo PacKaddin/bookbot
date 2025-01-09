7cd9b54042b643274e8335fb276760c24e30be5b
PacKaddin <papay@stud.uniza.sk>
2025-01-09 19:14:35 +0100

    update 2: bookbot finished

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    #print(text)
-    #get_word_count(text)
-    count_characters(text)
+    make_a_report(book_path)
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,9 +10,10 @@ def get_book_text(path):
 def get_word_count(text):
     words = text.split()
     count = len(words)
-    print(f"\n The amount of words in the text is: {count}")
+    #print(f"\n The amount of words in the text is: {count}")
+    return count
 
-def count_characters(text):
+def get_char_dict(text):
     amount = {}
     lowercase = text.lower()
     for character in lowercase:
@@ -22,6 +21,31 @@ def count_characters(text):
             amount[character] += 1
         else:
             amount[character] = 1
-    print(amount)
+    return amount
 
+def sort_on(dictionary):
+    return dictionary["num"]
+
+def make_a_report(path):
+    text = get_book_text(path)
+    count = get_word_count(text)
+    dictionary = get_char_dict(text)
+    print(f"--- Begin report of {path} ---")
+    print(f"{count} words were found in the document \n")
+    
+    entries = []
+    for key in dictionary:
+        entries.append({"name":key, "num":dictionary[key]})
+    entries.sort(reverse=True, key=sort_on)
+
+    for character in entries:
+        if (character["name"].isalpha()):
+            print(f"The {character["name"]} character was found {character["num"]} times")
+    
+    print("--- End report ---")
+
+    #If you wanna read the book, delete the commentary:
+    #print(text)
+
+    
 main()
